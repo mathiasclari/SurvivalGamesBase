df1543e37c06a9105467ce9397c8d8e3c2b47239
Mat <silencekrakens@gmail.com>
2022-03-01 01:12:15 +0100

    Initial commit

---
+++ src/main/java/net/acticraft/pixelcategorysg/PixelCategorySG.java
@@ -0,0 +1,17 @@
+package net.acticraft.pixelcategorysg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PixelCategorySG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
